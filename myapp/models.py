8ac9598e79f9648218f0933bb46e12956c0fa911
Edgar Avelar <edgareel@gmail.com>
2025-11-09 22:43:24 -0600

    hotfix venta

--- myapp/models.py
+++ myapp/models.py
@@ -376,6 +376,7 @@ class Venta(models.Model):
     cantidad_boletos = models.PositiveIntegerField()
     total_venta = models.DecimalField(max_digits=10, decimal_places=2)
     formato = models.CharField(max_length=10, null=True, blank=True)  # 2D, 3D, IMAX
+    fecha_venta = models.DateField(auto_now_add=True)  # Fecha en que se realizó la venta
 
     def __str__(self):
         return f"{self.pelicula.nombre} - {self.fecha}"
